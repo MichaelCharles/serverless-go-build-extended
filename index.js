0a02275d2687b606e87315174fbb8d0236526ff1
Michael Charles Aubrey <michael@akita-itlabo.com>
2021-05-10 17:11:27 +0900

    add option types

--- index.js
+++ index.js
@@ -75,6 +75,8 @@ class ServerlessPlugin {
             usage:
               'Not yet active: If the build should be made for the local machine (otherwise defaults to AWS deployment)',
             shortcut: 'l',
+            required: false,
+            type: 'boolean', 
           },
         },
       },
